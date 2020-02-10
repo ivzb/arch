a84a1d17f9c1c7b9e36c151e39076c762dacf985
ivzb <izahariev96@gmail.com>
2020-02-10 23:30:59 +0200

    send intent filter

--- app/src/main/java/com/ivzb/arch/ui/launcher/LauncherActivity.kt
+++ app/src/main/java/com/ivzb/arch/ui/launcher/LauncherActivity.kt
@@ -1,7 +1,10 @@
 package com.ivzb.arch.ui.launcher
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.ivzb.arch.domain.EventObserver
 import com.ivzb.arch.ui.main.MainActivity
@@ -10,6 +13,7 @@ import com.ivzb.arch.util.checkAllMatched
 import com.ivzb.arch.util.viewModelProvider
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
+import java.net.URL
 
 /**
  * A 'Trampoline' activity for sending users to an appropriate screen on launch.
@@ -30,5 +34,38 @@ class LauncherActivity : DaggerAppCompatActivity() {
             }.checkAllMatched
             finish()
         })
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    handleSendText(intent) // Handle text being sent
+                } else if (intent.type?.startsWith("image/") == true) {
+                    handleSendImage(intent) // Handle single image being sent
+                }
+            }
+
+            intent?.action == Intent.ACTION_SEND_MULTIPLE
+                    && intent.type?.startsWith("image/") == true -> {
+                handleSendMultipleImages(intent) // Handle multiple images being sent
+            }
+        }
+    }
+
+    private fun handleSendText(intent: Intent) {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            Log.d("arch", it)
+        }
+    }
+
+    private fun handleSendImage(intent: Intent) {
+        (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+            Log.d("arch", it.toString())
+        }
+    }
+
+    private fun handleSendMultipleImages(intent: Intent) {
+        intent.getParcelableArrayListExtra<Parcelable>(Intent.EXTRA_STREAM)?.let {
+            Log.d("arch", it.toString())
+        }
     }
 }
