d3ef74595429e773c4a784f0e203c5b8b65499c6
ivzb <izahariev96@gmail.com>
2020-02-16 20:37:32 +0200

    extract url from send intent

--- app/src/main/java/com/ivzb/arch/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/com/ivzb/arch/ui/main/MainActivityViewModel.kt
@@ -1,12 +1,14 @@
 package com.ivzb.arch.ui.main
 
 import android.content.Intent
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
 import com.ivzb.arch.domain.links.FetchLinkMetaDataUseCase
 import com.ivzb.arch.domain.links.InsertLinkUseCase
 import com.ivzb.arch.model.Link
 import com.ivzb.arch.ui.theme.ThemedActivityDelegate
 import javax.inject.Inject
+
 
 class MainActivityViewModel @Inject constructor(
     themedActivityDelegate: ThemedActivityDelegate,
@@ -16,10 +18,16 @@ class MainActivityViewModel @Inject constructor(
 
     fun handleLink(intent: Intent) {
         intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-            Link(url = it).let {
+            Link(url = extractUrl(it)).let {
                 insertLinkUseCase(it)
                 fetchLinkMetaDataUseCase(it)
             }
         }
     }
+
+    private fun extractUrl(input: String) =
+        input
+            .split(" ")
+            .firstOrNull { Patterns.WEB_URL.matcher(it).find() }
+            ?: input
 }
